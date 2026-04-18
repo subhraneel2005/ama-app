2362420027e54e15dc9def4d77b30ec886104458
subhraneel2005 <digitalsubhraneel@gmail.com>
2026-04-19 01:31:13 +0530

    added google native oauth

---
+++ app/api/auth/google/login/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+
+interface RedirectResponse {
+    success: boolean,
+    finalUrl? : string,
+    error?: string
+}
+
+export async function POST(req: NextRequest) {
+  const GOOGLE_CALLABACK_URL = "https://accounts.google.com/o/oauth2/v2/auth";
+  const GOOGLE_LOGIN_REDIRECT_URL =
+    "http://localhost:3000/api/auth/google/callback";
+
+  try {
+    const params = new URLSearchParams({
+      client_id: process.env.GOOGLE_CLIENT_ID!,
+      redirect_uri: GOOGLE_LOGIN_REDIRECT_URL,
+      response_type: "code",
+      scope: "openid email profile",
+      access_type: "offline",
+      prompt: "consent",
+    });
+
+    const finalUrl = `${GOOGLE_CALLABACK_URL}?${params.toString()}`;
+
+    return NextResponse.json<RedirectResponse>({
+      success: true,
+      finalUrl,
+    });
+  } catch (error) {
+    console.error("server error at login router", error);
+    return NextResponse.json<RedirectResponse>({
+      success: false,
+      error: "server error at login router",
+    });
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,23 @@
+"use client"
+
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+
+
+  const googleLoginHandler = async() => {
+    const res = await fetch("/api/auth/google/login", { method: "POST"})
+
+    if(!res.ok) return
+
+    const { finalUrl } = await res.json()
+
+    window.location.href = finalUrl
+  }
   return (
-    <div>Home</div>
-  )
+    <div className="min-h-screen w-full justify-center items-center flex flex-col">
+      <Button onClick={googleLoginHandler}>Continue with Google</Button>
+    </div>
+  );
 }
